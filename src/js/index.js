40b7bbe1e700c057ad2be71b3c0783f669a270d5
Razvan Popa <razvan.popa@softvision.ro>
2019-08-22 16:57:15 +0300

    3 - webpack/babel config and fetch data

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,32 @@
 // Global app controller
-const x = 23;
+// import str from './modules/Search';
 
-console.log(`constanta x = ${x}`);
+//import {add, multiply as m, id} from './view/searchView';
+//console.log(`testare ${add(2,5)} si id: ${id} si multiply: ${m(id, 10)}`);
+
+// import * as searchView from './view/searchView';
+
+// console.log(`testare ${searchView.add(2,5)} si id: ${searchView.id} si multiply: ${searchView.multiply(searchView.id, 10)}`);
+
+import axios from 'axios';
+
+async function getResults(query){
+    const key = '0a7feb282a2bf222d70cd7446f37ec77';
+    try{
+
+    const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+    const recipes = res.data.recipes;
+
+    // console.log(res);
+    // console.log(recipes);
+
+
+    } catch(error) {
+        alert(error);
+    }
+}
+
+// getResults('pizza');
+
+// https://www.food2fork.com/api/search
+// 0a7feb282a2bf222d70cd7446f37ec77

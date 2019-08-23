9b1fa539fc91ae35201ae337e97c0af585021c13
Razluc <razvancristianpopa@gmail.com>
2019-08-23 13:35:39 +0300

    4 - Search module

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,9 @@
 // Global app controller
-// import str from './modules/Search';
+import Search from './modules/Search';
 
-//import {add, multiply as m, id} from './view/searchView';
-//console.log(`testare ${add(2,5)} si id: ${id} si multiply: ${m(id, 10)}`);
-
-// import * as searchView from './view/searchView';
-
-// console.log(`testare ${searchView.add(2,5)} si id: ${searchView.id} si multiply: ${searchView.multiply(searchView.id, 10)}`);
-
-import axios from 'axios';
-
-async function getResults(query){
-    const key = '0a7feb282a2bf222d70cd7446f37ec77';
-    try{
-
-    const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-    const recipes = res.data.recipes;
-
-    // console.log(res);
-    // console.log(recipes);
-
-
-    } catch(error) {
-        alert(error);
-    }
-}
-
-// getResults('pizza');
+const search = new Search('pizza');
+// console.log(search);
+search.getResults();
 
 // https://www.food2fork.com/api/search
 // 0a7feb282a2bf222d70cd7446f37ec77
--- src/js/modules/Search.js
+++ src/js/modules/Search.js
@@ -1,1 +1,20 @@
-export default 'Hey';
+import axios from 'axios';
+
+export default class Search{
+    constructor(query){
+        this.query = query;
+    }
+
+    async getResults(){
+        const key = '0a7feb282a2bf222d70cd7446f37ec77';
+        try{
+            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            //console.log(res);
+            this.recipes = res.data.recipes;
+            //console.log(this.recipes);
+        } catch(error) {
+            alert(error);
+        }
+    }
+}
+
